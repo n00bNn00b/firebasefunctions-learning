544b5e49574ba765855dbe4a7b5ba83ae4550e3b
n00bNn00b <reldewan9@gmail.com>
2022-07-28 15:34:13 +0600

    daisyui configure

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,30 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        firebasetheme: {
+          primary: "#e298cd",
+
+          secondary: "#7facd8",
+
+          accent: "#f2a7b6",
+
+          neutral: "#2D2C3A",
+
+          "base-100": "#FDFBFD",
+
+          info: "#477BDC",
+
+          success: "#18776A",
+
+          warning: "#BB6D07",
+
+          error: "#F82912",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
